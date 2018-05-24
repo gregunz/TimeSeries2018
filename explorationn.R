06865f1649695904cec31f7ed3ccf6f4e8b0d264
valrigori <valentin.rigori@epfl.ch>
2018-05-24 17:35:34 +0200

    new version with plots

--- explorationn.R
+++ explorationn.R
@@ -21,31 +21,28 @@ Acf(co2, lag.max = 100); title('b) ACF', line=0.3)
 spectrum(ajgr$CO2, ylab="Periodogram", xlab="Frequency", main='')
 
 
-#-------------- Diff Data --------------#
-co2_d <- diff(co2, lag=24)
-kpss.test(co2_d)
-
-par(mar=c(3,3,1.2,0.1), mgp=c(1.5,0.4,0))
-par(mfrow=c(3,1))
-plot(diff(z), xlab="Time", ylab="Data diff"); title('a) Diff Data', line=0.3)
-Acf(co2_d, lag.max = 100); title('b) ACF', line=0.3)
-Pacf(co2_d, lag.max = 100); title('c) PACF', line=0.3)
-
-
 #-------------- Log data --------------#
 co2_log <- log(co2-300)
 plot(co2_log)
 kpss.test(co2_log)
 
+#-------------- Diff Data --------------#
+co2_d <- diff(co2, lag=24)
+kpss.test(co2_d)
 
-#-------------- dd data --------------#
+#-------------- Diff^2 data --------------#
 co2_dd <- diff(diff(co2, lag=24))
 kpss.test(co2_dd)
 
+par(mar=c(3,3,1.2,0.1), mgp=c(1.5,0.4,0))
+par(mfrow=c(3,1))
+plot(diff(z), xlab="Time", ylab="Data diff"); title('a) Diff Data', line=0.3)
+Acf(co2_dd, lag.max = 100); title('b) ACF', line=0.3)
+Pacf(co2_dd, lag.max = 100); title('c) PACF', line=0.3)
 
 
 # OUR MODEL
-co2_choosen <- co2_d
+co2_choosen <- co2_dd
 
 Acf(co2_choosen, lag.max = 100)
 Pacf(co2_choosen, lag.max = 100)
@@ -61,7 +58,6 @@ cpgram(e)
 # Ljung-Box
 lbt <- c(); for (h in 3:25) lbt[h] <- Box.test(e,lag=h,type='Ljung-Box',fitdf=2)$p.value
 plot(lbt, ylim=c(0,1)); abline(h=0.05,col='blue',lty='dotted')
-
 
 # forecasting
 fit0 <- Arima(co2, order=c(2, 1, 2), seasonal=list(order=c(0, 1, 1), period=24))
