9c0efbad7553a5014df9d6a92bf3d9a27d7253ea
valrigori <valentin.rigori@epfl.ch>
2018-05-29 16:03:46 +0200

    forecasting data + comparing with true values

--- explorationn.R
+++ explorationn.R
@@ -62,3 +62,21 @@ plot(lbt, ylim=c(0,1)); abline(h=0.05,col='blue',lty='dotted')
 # forecasting
 fit0 <- Arima(co2, order=c(2, 1, 2), seasonal=list(order=c(0, 1, 1), period=24))
 plot(forecast(fit0, h=100, level=95))
+
+fit1 <- Arima(co2[0:576], order=c(2, 1, 2), seasonal=list(order=c(0, 1, 1), period=24))
+f <- forecast(fit1, h=168, level=95)
+
+valuesForcasted <- f[["mean"]]
+trueValues <- co2[577:744]
+
+x <-  seq(1, 168, 1)
+
+plot(x, trueValues, type='l', col='blue', ylab="CO2 concentration")
+title('Predicted values vs true values')
+legend(135, 354, legend=c("True", "Predicted"), col=c("blue", "red"),
+       lty=1:1, cex=0.7)
+#axis(1, at=(1:length(x):10), labels = d[577:744])
+lines(x, valuesForcasted, col='red')
+
+
+
