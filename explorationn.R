be0396bddc58e238e0492f5b8d54e2ba8c8fc46f
valrigori <valentin.rigori@epfl.ch>
2018-06-05 20:45:45 +0200

    qqplot line + comparison of models

--- explorationn.R
+++ explorationn.R
@@ -45,29 +45,33 @@ Acf(co2_dd, lag.max = 100); title('b) ACF', line=0.3)
 Pacf(co2_dd, lag.max = 100); title('c) PACF', line=0.3)
 
 
-# OUR MODEL
+#-------------- OUR MODEL --------------#
 co2_choosen <- co2_dd
 
 Acf(co2_choosen, lag.max = 100)
 Pacf(co2_choosen, lag.max = 100)
 
-# => SAR
+# We fit a SARIMA model
 fit0 <- Arima(co2, order=c(2, 1, 2), seasonal=list(order=c(0, 1, 1), period=24))
 e <- fit0$residuals
 plot(e)
 Acf(e)
 Pacf(e)
 cpgram(e)
-qqnorm(e) #not good because we have extrem values
+qqnorm(e)
+qqline(e, col='red') #not good because we have extrem values
 
-# Ljung-Box
+#-------------- Ljung-Box --------------#
 lbt <- c(); for (h in 6:25) lbt[h] <- Box.test(e,lag=h,type='Ljung-Box',fitdf=5)$p.value
 plot(lbt, ylim=c(0,1)); abline(h=0.05,col='blue',lty='dotted')
 
-# forecasting
+
+
+#-------------- Forecasting --------------#
 fit0 <- Arima(co2, order=c(2, 1, 2), seasonal=list(order=c(0, 1, 1), period=24))
 plot(forecast(fit0, h=120, level=95))
 
+#-------------- Forecasting past values --------------#
 fit1 <- Arima(co2[1:576], order=c(2, 1, 2), seasonal=list(order=c(0, 1, 1), period=24))
 f <- forecast(fit1, h=168, level=95)
 
@@ -80,5 +84,13 @@ plot(x, trueValues, type='l', col='blue', ylab="CO2 concentration")
 title('Predicted values vs true values')
 legend(135, 354, legend=c("True", "Predicted"), col=c("blue", "red"),
        lty=1:1, cex=0.7)
-#axis(1, at=(1:length(x):10), labels = d[577:744])
 lines(x, valuesForcasted, col='red')
+
+
+
+#-------------- Comparison of models --------------#
+# do for each model:
+
+diff <- abs(trueValues - valuesForcasted)
+avg_error <- mean(diff)
+
